b5f9c46289200906381d614eddea35718fa3bfe9
Jaime Melis <j.melis@fdi.ucm.es>
2011-10-23 20:21:50 +0200

    Allow multiple origins

--- archiver.rb
+++ archiver.rb
@@ -11,7 +11,7 @@ require 'find'
 options = OpenStruct.new
 
 opts = OptionParser.new do |opts|
-  opts.banner = "Usage: archiver.rb [options] <origin> <target>"
+  opts.banner = "Usage: archiver.rb [options] <origin> [<origin> ...] <target>"
 
   opts.on("-n", "--dry-run", "Show what would be done.") do |n|
     options.dry_run = n
@@ -26,16 +26,15 @@ usage = opts.help
 
 opts.parse!
 
-if ARGV.length != 2
+if ARGV.length < 2
     puts usage
     exit 1
 end
 
-origin, target = ARGV
+paths = ARGV.map {|p| File.absolute_path(p)}
+origins, target = paths[0..-2], paths[-1]
 
-ORIGIN = File.absolute_path origin
-TARGET = File.absolute_path target
-DB     = File.join TARGET, 'files.db'
+DB = File.join target, 'files.db'
 
 ################################################################################
 # Libs
@@ -45,15 +44,17 @@ $: << ?.
 
 require 'ArchiverLib'
 
-Find.find(ORIGIN) do |path|
-    next unless File.file? path
+origins.each do |origin|
+  Find.find(origin) do |path|
+      next unless File.file? path
 
-    file = ArchiveFileUtil.new(path, ORIGIN, TARGET, options)
+      file = ArchiveFileUtil.new(path, origin, target, options)
 
-    if file.in_db?
-        file.remove
-    else
-        file.add_to_db
-        file.move
-    end
+      if file.in_db?
+          file.remove
+      else
+          file.add_to_db
+          file.move
+      end
+  end
 end
